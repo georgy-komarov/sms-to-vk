7d13616db0e03e81f44b9d7eda75475f1d21cf34
Georgy Komarov <georgy.komarov@mail.ru>
2019-10-16 21:25:09 +0300

    Initial commit

---
+++ src/db.py
@@ -0,0 +1,32 @@
+import json
+import subprocess
+import csv
+import pipes
+
+
+class DBHelper:
+    def __init__(self, path):
+        self.path = path
+
+    def query(self, query):
+        cmd = f'sqlite3 -csv -header {self.path} {pipes.quote(query)}'
+        termux_cmd = f'tsudo {cmd}'
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True, shell=True)
+        sqlite_out, sqlite_stderr = p.communicate()
+        reader = csv.DictReader(sqlite_out.split("\n"))
+        return list(reader)
+
+    def read_all_messages(self):
+        self.query('UPDATE sms SET read = 1')
+
+    def get_unread_messages(self):
+        messages = self.query('SELECT * FROM sms WHERE read = 0')
+        return messages
+
+    def get_last_messages(self, count: int):
+        return self.query(f'SELECT * FROM (SELECT * FROM sms ORDER BY _id DESC LIMIT {count}) Var1 ORDER BY _id')
+
+
+if __name__ == '__main__':
+    sms = DBHelper('../examples/mmssms.db')
+    json.dump(sms.get_unread_messages(), open('../examples/sms.json', 'w'), ensure_ascii=False, indent=2)
